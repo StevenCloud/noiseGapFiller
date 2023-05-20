81b609468d980e8154117f3d8ff07a3fa8e5268e
StevenCloud <stevencloud9@gmail.com>
2023-05-20 16:10:03 -0500

    working demo

--- noiseGapFiller.py
+++ noiseGapFiller.py
@@ -5,12 +5,12 @@ from pydub.playback import play
 from time import sleep
 
 # load the audio file of the AC sound
-ac_sound = pydub.AudioSegment.from_file("ac_sound.mp3")
+ac_sound = pydub.AudioSegment.from_file("ACNoise.wav")
 
 def audio_callback(indata, frames, time, status):
     volume_norm = np.linalg.norm(indata)*10
     print("Volume:", volume_norm)
-    if volume_norm < 65:
+    if volume_norm < 1.5:
         play(ac_sound)
 
 with sd.InputStream(callback=audio_callback):
